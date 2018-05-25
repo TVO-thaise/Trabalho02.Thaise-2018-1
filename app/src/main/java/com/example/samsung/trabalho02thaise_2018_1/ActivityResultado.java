3320d50054a75a596cdd0772707781400fefb4b8
TVO-thaise <thaisevilaca2013@gmail.com>
2018-05-25 11:20:36 -0300

    Class MainActivity, activity_main e ActivityResultado

---
+++ app/src/main/java/com/example/samsung/trabalho02thaise_2018_1/ActivityResultado.java
@@ -0,0 +1,45 @@
+package com.example.samsung.trabalho02thaise_2018_1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by Samsung on 25/05/2018.
+ */
+
+public class ActivityResultado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado);
+
+        Intent intent = getIntent();
+
+        String nome = (String) intent.getSerializableExtra("nome");
+        double salBruto = (double) intent.getSerializableExtra("salarioBruto");
+        double inss = (double) intent.getSerializableExtra("inss");
+        double sindicato = (double) intent.getSerializableExtra("sindicato");
+        double salLiquido = (double) intent.getSerializableExtra("salarioLiquido");
+
+
+
+        TextView tvNome = (TextView) findViewById(R.id.tvNome);
+        tvNome.setText("Nome: "+ nome);
+
+        TextView tvSalarioBruto = (TextView) findViewById(R.id.tvSalBruto);
+        tvSalarioBruto.setText("Salário Bruto: "+ salBruto);
+
+        TextView tvInss = (TextView) findViewById(R.id.tvInss);
+        tvInss.setText("INSS: "+ inss);
+
+        TextView tvSindicato = (TextView) findViewById(R.id.tvSindicato);
+        tvSindicato.setText("Sindicato: "+ sindicato);
+
+        TextView tvSalarioLiquido = (TextView) findViewById(R.id.tvSalLiquido);
+        tvSalarioLiquido.setText("Salário Liquido: "+ salLiquido);
+
+    }
+}
--- app/src/main/java/com/example/samsung/trabalho02thaise_2018_1/MainActivity.java
+++ app/src/main/java/com/example/samsung/trabalho02thaise_2018_1/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.samsung.trabalho02thaise_2018_1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText cargo;
+    private EditText horaExtraTrab;
+    private EditText filhos;
+    private EditText faltas;
+    private Button calular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cargo = (RadioButton) findViewById(R.id.etCargo);
+        horaExtraTrab = (EditText) findViewById(R.id.etHorasExtraTrabalhadas);
+        filhos = (EditText) findViewById(R.id.etNfilhos);
+        faltas = (EditText) findViewById(R.id.etNfaltas);
+        calular = (Button) findViewById(R.id.btCalcular);
+
+        calular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double fHoraExtraTrab = Double.parseDouble(horaExtraTrab.getText().toString());
+                double salarioBase;
+                double salarioLiquido, ir, inss, sindicato;
+
+                horaExtraTrab = salarioBase/240 * 2;
+
+                faltas = salarioBase /30;
+                filhos = salarioBase * 0,03
+                inss = salarioBase * 0.10;
+                proventos = salarioBase + horaExtraTrab + num_filhos * filhos;
+                descontos = inss + faltas;
+
+                salarioLiquido = proventos - descontos;
+
+                Intent intent = new Intent(MainActivity.this, ActivityResultado.class);
+                intent.putExtra("salarioBase", salarioBase);
+                intent.putExtra("inss", inss);
+                intent.putExtra("sindicato", sindicato);
+                intent.putExtra("salarioLiquido", salarioLiquido);
+                startActivity(intent);
+            }
+
+        });
+
     }
+
 }
